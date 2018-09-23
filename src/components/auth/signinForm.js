384d75264ecfb5c0369950263bb8940eb3372334
Rory Letteney <rory.letteney@gmail.com>
2018-09-23 09:21:36 -0500

    Styled form fields

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
 import { FormTitle } from "../formTitle";
-import { FormInput } from '../formFields';
+import { FormInput } from "../formFields";
 
 class SigninForm extends Component {
   render() {
     return (
       <form className="sign-in-form">
         <FormTitle className="sign-in-form__title" text="Login" />
-        <Field className="sign-in-form__email" component={FormInput} name="email" type="email"/>
+        <Field
+          className="sign-in-form__email"
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          title="Email"
+          component={FormInput}
+        />
+        <Field
+          className="sign-in-form__password"
+          placeholder="Enter Password"
+          name="password"
+          type="password"
+          title="Password"
+          component={FormInput}
+        />
       </form>
     );
   }
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 
 export class FormInput extends Component {
   render() {
-    const { className, title } = this.props;
+    const { className, title, input, type, placeholder } = this.props;
     return (
       <div className="form-input">
-      <label>{title}</label>
-        <input className={`${className} form-input`} />
+      <label className="form-input__title">{title}</label>
+        <input 
+            className={`${className} form-input__input`}
+            type={type}
+            {...input}
+            placeholder={placeholder}
+        />
       </div>
     );
   }
