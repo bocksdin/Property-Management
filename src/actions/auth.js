af3464ff2f1216f7123c8c4a6942f837b043aca6
Rory Letteney <rory.letteney@gmail.com>
2018-09-24 08:07:02 -0500

    Set token in local storage

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,23 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { ROOT_URL } from '../config';
+import { ROOT_URL } from "../config";
 
-import {
-    AUTHENTICATE_USER
-} from './types';
+import { AUTHENTICATE_USER } from "./types";
 
 export function signUp(fields, success) {
-    return function(dispatch) {
-        axios.post(`${ROOT_URL}/signUp`, fields)
-            .then(response => {
-                dispatch({
-                    type: AUTHENTICATE_USER,
-                    payload: response.data
-                })
-                success();
-            })
-            .catch(err => {
-                if(err) { console.log(err) }
-            })
-    }
+  return function(dispatch) {
+    axios
+      .post(`${ROOT_URL}/signUp`, fields)
+      .then(response => {
+        const { token } = response.data;
+        localStorage.setItem("token", token);
+        dispatch({
+          type: AUTHENTICATE_USER,
+          payload: response.data
+        });
+        success();
+      })
+      .catch(err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  };
 }
