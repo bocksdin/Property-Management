0720efe2824b0c951ab206824e23e56254507b12
Rory Letteney <rory.letteney@gmail.com>
2018-09-24 10:46:08 -0500

    Setup tab nav grid

--- src/components/tabNav.js
+++ src/components/tabNav.js
@@ -4,7 +4,14 @@ class TabNav extends Component {
   render() {
     return (
         <div className="tab-nav">
-    
+            <div className="tab-nav__tabs">
+                {
+                    this.props.tabs.map((tab, index) => {
+                        return <a className="tab-nav__tab">{tab.title}</a>
+                    })
+                }
+            </div>
+            <div>requests or newsletters content goes here</div>
         </div>
     )
   }
