b939017f411b4e033a5d964aa4fe5fecb3d0b3c6
Rory Letteney <rory.letteney@gmail.com>
2018-09-26 17:10:37 -0500

    Built icon component

---
+++ src/components/icon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Icon({icon}) {
+    return (
+        <i className={icon}/>
+    )
+}
+
+export default Icon;
--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Icon from '../icon';
+
 class RequestsItem extends Component {
     render() {
         return (
             <div className="requests-item">
-                item goes here
+                <div>
+                    <Icon icon="fas fa-exclamation-triangle"/>
+                </div>
             </div>
         )
     }
