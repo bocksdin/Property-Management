2d23da556ba36ebb1233f01abec337730824e058
Rory Letteney <rory.letteney@gmail.com>
2018-09-26 09:18:00 -0500

    Cancel button for newsletter archive detail component

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -1,33 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
-import NewsletterBox from './newsletterBox';
-import NewsletterLatest from './newsletterLatest';
+import NewsletterBox from "./newsletterBox";
+import NewsletterLatest from "./newsletterLatest";
 
-import { FormTitle } from '../formTitle';
+import { FormTitle } from "../formTitle";
+import { FormButton } from "../formFields";
 
 class NewsletterDetail extends Component {
+  componentDidMount() {
+    this.props.fetchNewsletterWithId(this.props.match.params.id);
+  }
 
-    componentDidMount() {
-        this.props.fetchNewsletterWithId(this.props.match.params.id);
-    }
-
-    render() {
-        console.log(this.props.newsletterToEdit);
-        return (
-            <div className='newsletter-detail'>
-                <FormTitle className="newsletter-detail__title" text="Newsletter Detail"/>
-                <NewsletterBox date={this.props.newsletterToEdit.date}/>
-                <NewsletterLatest {...this.props.newsletterToEdit}/>
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props.newsletterToEdit);
+    return (
+      <div className="newsletter-detail">
+        <FormTitle
+          className="newsletter-detail__title"
+          text="Newsletter Detail"
+        />
+        <NewsletterBox date={this.props.newsletterToEdit.date} />
+        <NewsletterLatest {...this.props.newsletterToEdit} />
+        <FormButton
+          className="newsletter-detail__cancel"
+          small={true}
+          onClick={() => this.props.history.push("/dashboard")}
+          title="Cancel"
+        />
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const {newsletterToEdit} = state.newsletters
-    return {newsletterToEdit};
+  const { newsletterToEdit } = state.newsletters;
+  return { newsletterToEdit };
 }
 
-export default connect(mapStateToProps, actions)(NewsletterDetail);
+export default connect(
+  mapStateToProps,
+  actions
+)(NewsletterDetail);
