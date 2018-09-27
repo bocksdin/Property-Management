c4d332aef1531d0a8d7bec0df5833069cef7e75f
Rory Letteney <rory.letteney@gmail.com>
2018-09-27 08:19:47 -0500

    Cleaned up request item grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -9,12 +9,12 @@ class RequestsItem extends Component {
             <div className="requests-item">
                 <Icon className="requests-item__icon" icon="fas fa-exclamation-triangle"/>
                 <div className="requests-item__title">
-                    My door fell down
+                    <div className="requests-item__title__text">My door fell down</div>
+                    <Icon className="requests-item__title__arrow" icon="fas fa-sort-down"/>
                 </div>
                 <div className="requests-item__tenant-unit">
                     Rory - Unit 115
                 </div>
-                <Icon className="requests-item__arrow" icon="fas fa-sort-down"/>
                 <div className="requests-item__date">
                     01/31/1990
                 </div>
