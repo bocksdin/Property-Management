27f450ee43b46ecc53d051d87596f45c270681e2
Rory Letteney <rory.letteney@gmail.com>
2018-10-01 08:11:08 -0500

    Provided newsletteredit.js component with fields one and two titles

--- src/components/newsletter/newsletterEdit.js
+++ src/components/newsletter/newsletterEdit.js
@@ -30,6 +30,8 @@ class EditNewsletter extends Component {
           onCancel={() => this.onCancel()}
           onSubmit={event => this.onSubmit(event)}
           formTitle="Edit Newsletter"
+          fieldOneTitle="Newsletter Title"
+          fieldTwoTitle="Newsletter Body"
         />
       </div>
     );
